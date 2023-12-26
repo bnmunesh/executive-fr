4b5faac717344c3369c47950386c74061181cbbf
Munesh <muneshkumarbn020@gmail.com>
2023-12-26 21:55:50 +0530

    first commit

---
+++ src/cht.jsx
@@ -0,0 +1,154 @@
+// CustomerSupportChat.js
+import React, { useState } from 'react';
+import './customer-support-chat.css';
+
+const CustomerSupportChat = () => {
+  // Sample data for conversations
+  const [conversations, setConversations] = useState([
+    { id: 1, status: 'ongoing', messages: ['Hello!', 'How can we assist you today?'] },
+    { id: 2, status: 'waiting', messages: [] },
+    { id: 3, status: 'closed', messages: [] },
+    // ... additional conversations ...
+  ]);
+
+  // State to keep track of the selected conversation
+  const [selectedConversation, setSelectedConversation] = useState(null);
+
+  // State for search bar
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // State for status filters
+  const [statusFilters, setStatusFilters] = useState({
+    ongoing: false,
+    waiting: false,
+    closed: false,
+  });
+
+  // State for new message input
+  const [newMessage, setNewMessage] = useState('');
+
+  // Function to handle conversation selection
+  const handleConversationSelect = (conversationId) => {
+    const selectedConv = conversations.find((conv) => conv.id === conversationId);
+    setSelectedConversation(selectedConv);
+  };
+
+  // Function to filter conversations based on search term and status filters
+  const filteredConversations = conversations.filter((conversation) => {
+    const hasSearchTerm = conversation.id.toString().includes(searchTerm);
+    const isFiltered =
+      (statusFilters.ongoing && conversation.status === 'ongoing') ||
+      (statusFilters.waiting && conversation.status === 'waiting') ||
+      (statusFilters.closed && conversation.status === 'closed');
+
+    return hasSearchTerm && (isFiltered || !statusFilters.ongoing && !statusFilters.waiting && !statusFilters.closed);
+  });
+
+  // Function to handle status filter changes
+  const handleStatusFilterChange = (status) => {
+    setStatusFilters((prevFilters) => ({ ...prevFilters, [status]: !prevFilters[status] }));
+  };
+
+  // Function to handle sending a new message
+  const handleSendMessage = () => {
+    if (newMessage.trim() !== '') {
+      const updatedConversations = conversations.map((conv) =>
+        conv.id === selectedConversation.id
+          ? { ...conv, messages: [...conv.messages, newMessage] }
+          : conv
+      );
+      setConversations(updatedConversations);
+      setNewMessage('');
+    }
+  };
+
+  return (
+    <div className="chat-container">
+      {/* Left Column */}
+      <div className="conversations-container">
+        <h2>Conversations</h2>
+
+        {/* Search Bar */}
+        <input
+          type="text"
+          placeholder="Search by ID"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+        {/* Add Box with Check Options for Status Filters */}
+        <div className="status-filters">
+          <label>
+            <input
+              type="checkbox"
+              checked={statusFilters.ongoing}
+              onChange={() => handleStatusFilterChange('ongoing')}
+            />
+            Ongoing
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={statusFilters.waiting}
+              onChange={() => handleStatusFilterChange('waiting')}
+            />
+            Waiting
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={statusFilters.closed}
+              onChange={() => handleStatusFilterChange('closed')}
+            />
+            Closed
+          </label>
+        </div>
+
+        <ul className="conversation-list">
+          {filteredConversations.map((conversation) => (
+            <div
+              key={conversation.id}
+              className={`conversation-item ${selectedConversation?.id === conversation.id ? 'selected' : ''}`}
+              onClick={() => handleConversationSelect(conversation.id)}
+            >
+              <div>{`ID: ${conversation.id}`}</div>
+              <div>Status: {conversation.status}</div>
+            </div>
+          ))}
+        </ul>
+      </div>
+
+      {/* Right Column */}
+      <div className="chat-window">
+        {selectedConversation ? (
+          <>
+            <h2 className="chat-header">{`Conversation ID: ${selectedConversation.id} - Status: ${selectedConversation.status}`}</h2>
+            <div className="message-container">
+              {/* Display messages for the selected conversation */}
+              {selectedConversation.messages.map((message, index) => (
+                <div key={index} className="message-item">
+                  {message}
+                </div>
+              ))}
+            </div>
+
+            {/* Type Message Box and Send Button */}
+            <div>
+              <input
+                type="text"
+                placeholder="Type your message..."
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+              />
+              <button onClick={handleSendMessage}>Send</button>
+            </div>
+          </>
+        ) : (
+          <p>Select a conversation to start chatting</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CustomerSupportChat;
